8dd2186db1f69a22aaa144724d1e47348ffa8bc7
michael <maiconxandroid@gmail.com>
2022-06-21 12:14:50 -0300

    Adicionado o diretótio do arquivo js no index.html, ajuste no style.css e criado código de cores aleatórias no script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let colorCircle = document.querySelector(".circle");
+
+let colorRandom = () => {
+    const RED = Math.floor(Math.random() * 255);
+    const GREEN = Math.floor(Math.random() * 255);
+    const BLUE = Math.floor(Math.random() * 255);
+
+    return `#${RED}${GREEN}${BLUE}`;
+}
+
+colorCircle.style.backgroundColor = colorRandom();
