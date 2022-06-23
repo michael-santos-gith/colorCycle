4b0e782c598f055ba2b4ae7ef2048f8ea8390703
michael <maiconxandroid@gmail.com>
2022-06-22 21:44:50 -0300

    Adicionado keyframes para a transição das coress e comentado o código js.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-let colorCircle = document.querySelector(".circle");
+// let colorCircle = document.querySelector(".circle");
 
-let colorRandom = () => {
-    const RED = Math.floor(Math.random() * 255);
-    const GREEN = Math.floor(Math.random() * 255);
-    const BLUE = Math.floor(Math.random() * 255);
+// let colorRandom = () => {
+//     const RED = Math.floor(Math.random() * 255);
+//     const GREEN = Math.floor(Math.random() * 255);
+//     const BLUE = Math.floor(Math.random() * 255);
 
-    return `#${RED}${GREEN}${BLUE}`;
-}
+//     return `#${RED}${GREEN}${BLUE}`;
+// }
 
-colorCircle.style.backgroundColor = colorRandom();
+// colorCircle.style.backgroundColor = colorRandom();
+// document.documentElement.style.setProperty("--color-01", colorRandom());
