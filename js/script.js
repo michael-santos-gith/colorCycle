6a16f5b411f41ab112bc2d9cd237d7c36a5bcd0e
michael <maiconxandroid@gmail.com>
2022-06-26 16:45:48 -0300

    Refatorando o código criando as funções decimalToHex e colorCycle.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 function random(min, max) {
-    const number = Math.floor(Math.random() * (max - min)) + min;
-    return number;
-  }
+  const number = Math.floor(Math.random() * (max - min)) + min;
+  return number;
+}
   
-  function randomColor() {
-    return `rgb(${random(0,255)}, ${random(0,255)}, ${random(0,255)})`;
-  }
+function randomColor() {
+  return `rgb(${random(0,255)}, ${random(0,255)}, ${random(0,255)})`;
+}
 
-document.documentElement.style.setProperty('--color-01', randomColor());
-document.documentElement.style.setProperty('--color-02', randomColor());
-document.documentElement.style.setProperty('--color-03', randomColor());
-document.documentElement.style.setProperty('--color-04', randomColor());
-document.documentElement.style.setProperty('--color-05', randomColor());
-document.documentElement.style.setProperty('--color-06', randomColor());
-document.documentElement.style.setProperty('--color-07', randomColor());
-document.documentElement.style.setProperty('--color-08', randomColor());
-document.documentElement.style.setProperty('--color-09', randomColor());
-document.documentElement.style.setProperty('--color-10', randomColor());
-document.documentElement.style.setProperty('--color-11', randomColor());
+function decimalToHex(func) {
+  let numberHex, RED, GREEN, BLUE;
+
+  numberHex = randomColor().toString();
+  numberHex = numberHex.replace(/\D+/g, ',');
+  numberHex = numberHex.slice(1);
+  [ RED, GREEN, BLUE ] = numberHex.split(',');
+
+  return console.log(RED,GREEN,BLUE);
+}
+
+function colorCycle() {
+  for (let index = 1; index <= 11; index++) {
+    document.documentElement.style.setProperty(`--color-0${index}`, randomColor());
+    decimalToHex(randomColor()); 
+  }
+}
+
+colorCycle();
